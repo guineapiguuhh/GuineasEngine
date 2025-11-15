aa0f821ffc503882a9d9915c297afd98bffba3ae
guineapiguuhh <pedromaxaretz@gmail.com>
2025-11-14 21:42:52 -0300

    debloated

--- GuineasEngine/Core.cs
+++ GuineasEngine/Core.cs
@@ -1,26 +1,41 @@
-using GuineasEngine.Utils;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GuineasEngine;
 
-public class NewGame : Game
+public class Core : Game
 {
+    public static Core Instance { get; private set; }
+
     public static GraphicsDeviceManager Graphics { get; private set; }
     public static new GraphicsDevice GraphicsDevice { get; private set; }
+
+    public static SpriteBatch SpriteBatch { get; private set; }
+
     public static float DeltaTime { get; private set; } = 0f;
 
-    public NewGame()
+    public Core()
     {
+        if (Instance is not null)
+        {
+            throw new Exception();
+        }
+        Instance = this;
+
         Graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = @"Content";
+    }
+
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
     }
 
     protected override void Initialize()
     {
         base.Initialize();
         GraphicsDevice = base.GraphicsDevice;
-        Render.Initialize();
     }
 
     protected override void Update(GameTime gameTime)
--- GuineasEngine/Node.cs
+++ GuineasEngine/Node.cs
@@ -1,23 +1,67 @@
-using GuineasEngine.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GuineasEngine;
 
-public class Node : IUpdateable, IDrawable
+public class Node : Components.IUpdateable, Components.IDrawable
 {
     public string Name = "Node";
 
-    internal Node Parent { get; set; }
+    public Node Parent { get; internal set; }
 
     protected List<Node> Children { get; set; }
+
+    #region Global
+    public Vector2 GlobalPosition
+    {
+        get
+        {
+            if (Parent is not null)
+            {
+                return ApplyPoint(Parent.GlobalPosition + Position, Parent.GlobalAngle);
+            }
+            return Position;
+        }
+    }
+
+    public Vector2 GlobalScale
+    {
+        get
+        {
+            if (Parent is not null)
+            {
+                return Parent.GlobalScale * Scale;
+            }
+            return Scale;
+        }
+    }
+
+    public float GlobalAngle
+    {
+        get
+        {
+            if (Parent is not null)
+            {
+                return Parent.Angle + Angle;
+            }
+            return Angle;
+        }
+    }
+    #endregion
+
+    public Vector2 Position = Vector2.Zero;
+    public Vector2 Origin = Vector2.Zero;
+    public Vector2 Scale = Vector2.One;
+    public float Angle = 0f;
+
+    public Color Color = Color.White;
+    public SpriteEffects Effect = SpriteEffects.None;
+
+    public float LayerDepth = 0f;
 
     public Node()
     {
         Children = [];
-    }
-
-    public Node GetParent()
-    {
-        return Parent;
     }
 
     public void AddChild(Node child)
@@ -26,24 +70,103 @@ public class Node : IUpdateable, IDrawable
         Children.Add(child);
     }
 
+    public void InsertChild(int index, Node child)
+    {
+        child.Parent = this;
+        Children.Insert(index, child);
+    }
+
     public void RemoveChild(Node child)
     {
         child.Parent = null;
         Children.Remove(child);
     }
 
+    public void ForEach(Action<Node> action)
+    {
+        for (int i = 0; i < Children.Count; i++)
+        {
+            action(Children[i]);
+        }
+    }
+
+    private Vector2 ApplyPoint(Vector2 value, float angle)
+    {
+        return new Vector2();
+    }
+
     public virtual void Update(float deltaTime)
     {
-        for (int i = 0; i < Children.Count; i++) Children[i].Update(deltaTime);
+        ForEach((child) => child.Update(deltaTime));
     }
 
     public virtual void Draw()
     {
-        for (int i = 0; i < Children.Count; i++) Children[i].Draw();
+        ForEach((child) => child.Draw());
     }
     
     public override string ToString()
     {
         return Name;
     }
+
+    #region Draw
+    protected void DrawTexture(
+        Texture2D texture,
+        Rectangle destinationRectangle
+    )
+    {
+        Core.SpriteBatch.Draw(texture, destinationRectangle, Color);
+    }
+
+    protected void DrawTexture(
+        Texture2D texture,
+        Rectangle destinationRectangle,
+        Rectangle sourceRectangle
+    )
+    {
+        Core.SpriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color);
+    }
+
+    protected void DrawTexture(
+        Texture2D texture,
+        Rectangle destinationRectangle,
+        Rectangle sourceRectangle,
+        float angle,
+        Vector2 origin,
+        SpriteEffects effects,
+        float layerDepth
+    )
+    {
+        Core.SpriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color, angle + GlobalAngle, origin, effects, layerDepth);
+    }
+
+    protected void DrawTexture(
+        Texture2D texture,
+        Vector2 position,
+        Rectangle sourceRectangle,
+        float angle,
+        Vector2 origin,
+        float scale,
+        SpriteEffects effects,
+        float layerDepth
+    )
+    {
+        Core.SpriteBatch.Draw(texture, position, sourceRectangle, Color, angle, origin, new Vector2(scale, scale), effects, layerDepth);
+    }
+
+    protected void DrawTexture(
+        Texture2D texture,
+        Vector2 position,
+        Rectangle sourceRectangle,
+        float angle,
+        Vector2 origin,
+        Vector2 scale,
+        SpriteEffects effects,
+        float layerDepth
+    )
+    {
+        Core.SpriteBatch.Draw(texture, position + GlobalPosition, sourceRectangle, Color, angle + GlobalAngle, origin, scale + GlobalScale, effects, layerDepth);
+    }
+    #endregion
 }
